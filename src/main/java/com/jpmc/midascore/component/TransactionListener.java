d0c04016f0c9ef6f65d09bd406c1364ac8032fd9
Aliyah Crumbley <aliyah.crumbley@gmail.com>
2024-11-10 19:46:11 -0500

    first commit

---
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -0,0 +1,16 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionListener {
+
+
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas")
+    public void listen(Transaction transaction) {
+
+    }
+
+}
